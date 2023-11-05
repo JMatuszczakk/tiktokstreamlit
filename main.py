788a1f190befd1a024a304244c272080ca8b49c2
Jakub Matuszczak <72751477+jkm2@users.noreply.github.com>
2023-11-05 19:49:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ url = st.text_input('Wpisz url tiktoka')
 chart = st.line_chart()
 current_stats = st.empty()
 progress_bar = st.progress(0)
-st.input("Podaj nazwe tiktoka jeśli chesz")
+st.text_input("Podaj nazwe tiktoka jeśli chesz")
 
 if url != '':
     video_id = re.search(r'(?<=video/)[^/]+', url).group()
